88d01522f6f32e49a38370855ce8ab4f9f9e874b
root <root@centos7-1.hemantvm.com>
2019-05-08 00:18:16 -0500

    grant or revoke ss^Caccess to new users for linux machines

---
+++ create_users.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USER_NAMES=`cat user_list_file.txt`
+HOST_NAMES=`cat host_list_file.txt`
+
+for USER_NAME in ${USER_NAMES}
+	do		
+		for HOST_NAME in ${HOST_NAMES}
+			do
+				if [[ $1 == "grant" ]]; then
+					useradd ${USER_NAME}
+					sudo -u ${USER_NAME} mkdir /home/${USER_NAME}/.ssh
+					sudo -u ${USER_NAME} chmod 700 /home/${USER_NAME}/.ssh
+					sudo -u ${USER_NAME} ssh-keygen -f /home/${USER_NAME}/.ssh/id_rsa -t rsa -N ''
+					ssh -o StrictHostKeyChecking=no root@${HOST_NAME} "useradd ${USER_NAME}"
+					ssh -o StrictHostKeyChecking=no root@${HOST_NAME} "mkdir /home/${USER_NAME}/.ssh"
+					ssh -o StrictHostKeyChecking=no root@${HOST_NAME} "chmod 700 /home/${USER_NAME}/.ssh"
+					ssh -o StrictHostKeyChecking=no root@${HOST_NAME} "touch /home/${USER_NAME}/.ssh/authorized_keys"
+					ssh -o StrictHostKeyChecking=no root@${HOST_NAME} "chmod 600 /home/${USER_NAME}/.ssh/authorized_keys"
+					#ssh-copy-id -i /home/${USER_NAME}/.ssh/id_rsa.pub ${USER_NAME}@${HOST_NAME}
+					echo `cat /home/${USER_NAME}/.ssh/id_rsa.pub` | ssh -o StrictHostKeyChecking=no root@${HOST_NAME} "cat >> /home/${USER_NAME}/.ssh/authorized_keys"
+				elif [[ $1 == "revoke" ]]; then
+					userdel -r ${USER_NAME}
+					ssh -o StrictHostKeyChecking=no root@${HOST_NAME} "userdel -r ${USER_NAME}"
+				fi
+			done
+	done
